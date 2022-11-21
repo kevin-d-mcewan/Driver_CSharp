28130f702fb09c33d60922296e06be96b6aaa240
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2022-11-20 19:40:08 -0800

    Finished the base gameplay of it.  Needs much polish and level design and UI as well. But it a playable game mechanics

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    // Color variable for car depending of having package or not
+    //   *** Make sure that in Inspector the 'hasPackage' & 'noPackage' the alpha is not zero or it will appear as if vanished ***
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(0, 0, 0, 0);
 
+    // Reference to SpriteRender so we can GetComponent
+    SpriteRenderer spriteRenderer;
+    
     bool hasPackage = false;
     bool hasMail = false;
-    // GameObject package;
+    
     [SerializeField] float destroyDelay = 0.5f;
+
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -19,17 +32,19 @@ public class Delivery : MonoBehaviour
     {
         // Other refers to the parameter that you are passing in for OnTriggerEnter2D() 'other' is just var name can be named anything
         // Debug.Log ("you have picked up 'anything');
-        if (other.tag == "Package" && !hasPackage)
+        if (other.tag == "Package" && !hasPackage && !hasMail)
         {
             Debug.Log("You have picked up the package.");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyDelay);
             
         } 
-        else if(other.tag == "Mail" && !hasMail)
+        else if(other.tag == "Mail" && !hasMail & !hasPackage)
         {
             Debug.Log("You have picked up the mail");
             hasMail = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyDelay);
 
         }
@@ -37,11 +52,13 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Delivered the package");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
         else if (other.tag == "Customer" && hasMail)
         {
             Debug.Log("Mail delivered!");
             hasMail = false;
+            spriteRenderer.color = noPackageColor;
         }
 
         
--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -6,6 +6,10 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 0.01f;   // Make sure to change these in the Inspector or else car wont move.
     [SerializeField] float steerSpeed = 1.0f;   // These speeds are without Time.dT those nums are more like 15 m.s. & 225 s.s.
+    [SerializeField] float slowSpeed = 15.0f;   // Is the 'moveSpeed' adjustment if going over a bump
+    [SerializeField] float boostSpeed = 35.0f;  // Is a speed boost that increase 'moveSpeed like arrows in MarioKart
+    [SerializeField] float normalSpeed = 20.0f; // Speed Car can go after boost or bump or crash
+    [SerializeField] float timeToNormal = 3.0f; // This is how long they have the speedBoost or speedBump for. Time amount can be changed in Inspector
 
     // Start is called before the first frame update
     void Start()
@@ -24,5 +28,42 @@ public class Driver : MonoBehaviour
         transform.Rotate(0, 0, -steerAmount);
         // To make the vehicle move Up & Down
         transform.Translate(0, moveAmount, 0);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Logic for speeding up or slowing down
+        if (collision.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+            StartCoroutine(Back_To_Normal(timeToNormal));
+        }
+
+        if (collision.tag == "Bump")
+        {
+            moveSpeed = slowSpeed;
+            StartCoroutine(Back_To_Normal(timeToNormal));
+        }
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Building" || other.gameObject.tag == "TreeAndPlant")
+        {
+            moveSpeed = slowSpeed;
+            StartCoroutine(Back_To_Normal(timeToNormal));
+        }
+    }
+
+
+    // This CoRoutine is used to change the speed back to the normal speed after a certain amount of time instead
+    // of just hitting another speedBoost or speedBump
+    IEnumerator Back_To_Normal(float time) 
+    {
+        yield return new WaitForSeconds(time);
+        moveSpeed = normalSpeed;
     }
 }
